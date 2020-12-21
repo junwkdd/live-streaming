262c68be71d1033cdcf2e947e54336d5bab17855
홍준석 <34802959+junwkdd@users.noreply.github.com>
2020-12-21 13:37:48 +0900

    Fix ffmpeg path

--- config/default.js
+++ config/default.js
@@ -13,7 +13,7 @@ const config = {
       allow_origin: '*',
     },
     trans: {
-      ffmpeg: 'C:/Program Files/ffmpeg/bin/ffmpeg.exe',
+      ffmpeg: '/usr/bin/ffmpeg',
       tasks: [
         {
           app: 'live',
